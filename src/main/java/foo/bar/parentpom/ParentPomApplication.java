e9cca60a92d4d5020b1ea97683e5ad2904ba968e
mahabaleshwars <mahabaleshwar.s@infosys.com>
2024-04-24 16:52:14 +0530

    initial commit

---
+++ src/main/java/foo/bar/parentpom/ParentPomApplication.java
@@ -0,0 +1,13 @@
+package foo.bar.parentpom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ParentPomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ParentPomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/foo/bar/parentpom/ParentPomApplicationTests.java
@@ -0,0 +1,13 @@
+package foo.bar.parentpom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParentPomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
